8daeba30430013a668ab3f1f2947789e0e731b0f
KSY_AI <suyun101014@gmail.com>
2025-03-31 22:46:32 +0900

    Update io.py

--- Baekjoon/step/io.py
+++ Baekjoon/step/io.py
@@ -1,22 +1,33 @@
+#
 print(f'Hello World!')
-
+#
 num1, num2 = map(int, input().split())
 print(f'{num1 + num2}')
-
+#
 num1, num2 = map(int, input().split())
 print(f'{num1 - num2}')
 
 num1, num2 = map(int, input().split())
 print(f'{num1 * num2}')
 
+
+
 num1, num2 = map(float, input().split())
 print(f'{num1/num2}')
-
+#10926
 name = input()
 print(f'{name}??!')
-
+#18108
 year = int(input())
 print(f'{year-543}')
-
+#10430
 A,B,C = map(int, input().split())
 print(f'{(A+B)%C}\n{((A%C)+(B%C)%C)}\n{(A*B)%C}\n{((A%C)*(B%C))%C}')
+#2588
+num1 = int(input())
+num2 = int(input())
+a = num2 // 100
+b = (num2//10) - (a*10)
+c = num2 - (a*100) - (b*10)
+print(f'{num1*c}\n{num1*b}\n{num1*c}\n{num1*num2}')
+#11382
