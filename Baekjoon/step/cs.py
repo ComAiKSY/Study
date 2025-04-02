0cb4cc92f4b46b724881472bb8af21b2fef0a322
KSY_AI <suyun101014@gmail.com>
2025-04-02 19:50:51 +0900

    Update cs.py

--- Baekjoon/step/cs.py
+++ Baekjoon/step/cs.py
@@ -26,3 +26,16 @@ if year%4==0 and year%100 !=0 or year%400==0:
     print('1')
 else:
     print('0')
+
+#14681
+x = int(input())
+y = int(input())
+
+if x>0 and y>0:
+    print('1')
+elif x < 0 < y:
+    print('2')
+elif x<0 and y<0:
+    print('3')
+else:
+    print('4')
