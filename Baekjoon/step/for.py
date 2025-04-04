7a739b50cde63ef26b6290ac7626c60585b7b263
KSY_AI <suyun101014@gmail.com>
2025-04-04 20:17:30 +0900

    Update for.py

--- Baekjoon/step/for.py
+++ Baekjoon/step/for.py
@@ -8,3 +8,24 @@ num = int(input())
 for _ in range(num):
     A, B = map(int, input().split())
     print(f'{A+B}')
+
+#8393
+n = int(input())
+sum = 0
+for i in range(1,n+1):
+    sum += i
+print(sum)
+
+#25304
+X = int(input())
+type = int(input())
+price =[0]*type
+num = [0]*type
+total = 0
+for i in range(type):
+    price[i], num[i] = map(int, input().split())
+    total += price[i]*num[i]
+if X == total:
+    print(f'Yes')
+else:
+    print(f'No')
