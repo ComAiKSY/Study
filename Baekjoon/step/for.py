92b676abe56b9ced4a0b32dfa53df797ab677f5a
KSY_AI <suyun101014@gmail.com>
2025-04-04 20:08:51 +0900

    Update for.py

--- Baekjoon/step/for.py
+++ Baekjoon/step/for.py
@@ -1,3 +1,10 @@
+#2739
 N = int(input())
 for i in range(1,10):
     print(f'{N} * {i} = {N*i}')
+
+#10950
+num = int(input())
+for _ in range(num):
+    A, B = map(int, input().split())
+    print(f'{A+B}')
