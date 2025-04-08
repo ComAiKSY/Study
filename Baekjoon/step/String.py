5bd77557081d92acd10528e435d7618b32b3637f
KSY_AI <suyun101014@gmail.com>
2025-04-09 00:04:49 +0900

    Update String.py

--- Baekjoon/step/String.py
+++ Baekjoon/step/String.py
@@ -1,4 +1,6 @@
 #27866
+import sys
+
 word = str(sys.stdin.readline().strip())
 array = list(word)
 i = int(sys.stdin.readline())
@@ -6,5 +8,17 @@ print(array[i-1])
 
 #2743
 word = str(sys.stdin.readline().strip())
-lenght = len(word)
-print(lenght)
+length = len(word)
+print(length)
+
+#9086
+T = int(sys.stdin.readline())
+result = [0]*T
+for i in range(T):
+    W = str(sys.stdin.readline().strip())
+    length = len(W)
+    array = list(W)
+    result[i] = array[0] + array[length-1]
+
+for i in range(T):
+    print(result[i])
