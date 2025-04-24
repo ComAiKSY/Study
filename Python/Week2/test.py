8375c421a76094f7f785b027cb64e4fef15dfa86
KSY_AI <suyun101014@gmail.com>
2025-04-25 02:03:09 +0900

    Quiz_2week

--- Python/Week2/test.py
+++ Python/Week2/test.py
@@ -4,3 +4,7 @@ secondvalue = int(input('Enter second value: '))
 firstvalue = mainvalue // secondvalue
 result = mainvalue - (secondvalue * firstvalue)
 print(result)
+
+num1 = int(input("숫자 1 ==> "))
+num2 = int(input("숫자 2 ==> "))
+print(f'{num1} * {num2} = {num1*num2}')
