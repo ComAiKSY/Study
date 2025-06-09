fd8dcdb0b84c1505400357d1a5ab71f71efbd2a3
KSY_AI <suyun101014@gmail.com>
2025-06-10 01:04:03 +0900

    Update 3_7_result.cpp

--- Graphics/3_7_result.cpp
+++ Graphics/3_7_result.cpp
@@ -1,83 +1,152 @@
 #include <GL/glut.h>
+#include <stdio.h>
 #include <iostream>
 #include <cmath>
-
 #define GL_PI 3.1415f
 
-int count = 0;  // 보여줄 조각 수 (1~8)
+bool bCull = 1;
+int num = 0;
 
-void RenderScene(void) {
-    glClear(GL_COLOR_BUFFER_BIT);
+GLfloat xTran = 0.0f;
+GLfloat yTran = 0.0f;
+GLfloat xRot = 0.0f;
+GLfloat yRot = 0.0f;
 
-    int num = 8;
-    float radius = 50.0f;
+void SpecialKeys(int key, int x, int y) {
+	if (key == GLUT_KEY_UP)
+		xRot -= 2.0f;
+	else if (key == GLUT_KEY_DOWN)
+		xRot += 2.0f;
+	else if (key == GLUT_KEY_LEFT)
+		yRot -= 2.0f;
+	else if (key == GLUT_KEY_RIGHT)
+		yRot += 2.0f;
 
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(0.0f, 0.0f);  // 중심점
+	if (xRot > 360.0f)
+		xRot -= 360.0f;
+	if (xRot < 0.0f)
+		xRot += 360.0f;
+	if (yRot > 360.0f)
+		yRot -= 360.0f;
+	if (yRot < 0.0f)
+		yRot += 360.0f;
 
-    for (int i = 0; i <= count; i++) {
-        float angle = 2.0f * GL_PI * i / num;
-        float x = cos(angle) * radius;
-        float y = sin(angle) * radius;
-
-        if (i % 2 == 0)
-            glColor3f(0.0f, 1.0f, 0.0f);  // 초록
-        else
-            glColor3f(1.0f, 0.0f, 0.0f);  // 빨강
-
-        glVertex2f(x, y);
-    }
-
-    glEnd();
-
-    glutSwapBuffers();
-    glFlush();
+	glutPostRedisplay();
 }
 
 void keyboard(unsigned char key, int x, int y) {
-    if (key >= '1' && key <= '8') {
-        count = key - '0';        // '1' → 1, ..., '8' → 8
-        glutPostRedisplay();      // 다시 그리기 요청
-    }
+	if (key == 'a')
+		xTran -= 2.0f;
+	else if (key == 'd')
+		xTran += 2.0f;
+	else if (key == 'w')
+		yTran += 2.0f;
+	else if (key == 's')
+		yTran -= 2.0f;
+	else if (key == '1')
+		num = 1;
+	else if (key == '2')
+		num = 2;
+	else if (key == '3')
+		num = 3;
+	else if (key == '4')
+		num = 4;
+	else if (key == '5')
+		num = 5;
+	else if (key == '6')
+		num = 6;
+	else if (key == '7')
+		num = 7;
+	else if (key == '8')
+		num = 8; 
+	glutPostRedisplay();
+}
+
+void RenderScene(void) {
+
+	int cnt = 0;
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	if (bCull)
+		glEnable(GL_CULL_FACE);
+	else
+		glDisable(GL_CULL_FACE);
+
+	glPushMatrix();
+	glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+
+	glTranslatef(xTran, yTran, 0.0f);
+
+	GLfloat x, y, angle;
+	int iPivot = 1;
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(0.0f, 0.0f);
+
+	for (angle = 0.0f; angle < (2.05f * GL_PI); angle += (GL_PI / 4.0f)) {
+
+		if (cnt == num+1)
+			break;
+
+		x = 50.0f * cos(angle);
+		y = 50.0f * sin(angle);
+
+		if ((iPivot % 2) == 0)
+			glColor3f(0.0f, 1.0f, 0.0f);
+		else
+			glColor3f(1.0f, 0.0f, 0.0f);
+
+		cnt++;
+		iPivot++;
+		glVertex2f(x, y);
+	}
+	glEnd();
+	glPopMatrix();
+	glutSwapBuffers();
 }
 
 void SetupRC(void) {
-    std::cout << "SetupRC" << std::endl;
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glShadeModel(GL_FLAT);
+	// std::cout << "SetupRC" << std::endl;
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glShadeModel(GL_FLAT);
+
 }
 
 void ChangeSize(GLsizei w, GLsizei h) {
-    std::cout << "w = " << w << " " << " h = " << h << std::endl;
+	// std::cout << "w = " << w << " " << " h = " << h << std::endl;
+	// std::cout << "ChangeSize" << std::endl;
 
-    GLfloat aspectRatio;
+	GLint nRange = 100.0f;
+	GLfloat aspectRatio;
+	if (h == 0) {
+		h = 1;
+	}
+	glViewport(0, 0, w, h);
 
-    if (h == 0) h = 1;
-    glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+	aspectRatio = (GLfloat)w / (GLfloat)h;
+	if (w <= h)
+		glOrtho(-nRange, nRange, -nRange / aspectRatio, nRange / aspectRatio, nRange, -nRange);
+	else
+		glOrtho(-nRange * aspectRatio, nRange * aspectRatio, -nRange, nRange, nRange, -nRange);
 
-    aspectRatio = (GLfloat)w / (GLfloat)h;
-    if (w <= h)
-        glOrtho(-100, 100, -100 / aspectRatio, 100 / aspectRatio, -500, 500);
-    else
-        glOrtho(-100 * aspectRatio, 100 * aspectRatio, -100, 100, -500, 500);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(400, 400);
-    glutCreateWindow("Redner");  // 창 제목 고정
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(400, 400);
+	glutCreateWindow("simple");
 
-    SetupRC();
-    glutDisplayFunc(RenderScene);
-    glutReshapeFunc(ChangeSize);
-    glutKeyboardFunc(keyboard);
-    glutMainLoop();
+	SetupRC();
+	glutDisplayFunc(RenderScene);
+	glutReshapeFunc(ChangeSize);
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(SpecialKeys);
+	glutMainLoop();
 }
