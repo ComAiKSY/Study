25fb8d45d9dfc98aefe3d627ef292db3530eead5
KSY_AI <suyun101014@gmail.com>
2025-09-17 21:08:34 +0900

    Update and.py

--- DR/and.py
+++ DR/and.py
@@ -1,12 +1,25 @@
 import numpy as np
-    def AND(x1, x2):
-        w1, w2, theta = 0.5, 0.5, 0.7
-        tmp = w1*x1 + w2*x2
-        if tmp <= theta:
-            return 0
-        else:
-            return 1
-    AND(0, 0)
-    AND(0, 1)
-    AND(1, 0)
-    AND(1, 1)
+ def NAND(x1, x2):
+  x = np.array([x1, x2])
+  w = np.array([-0.5, -0.5])
+  b = 0.7
+  tmp = np.sum(w*x) + b
+  if tmp <= 0:
+   return 0
+  else:
+   return 1
+ def OR(x1, x2):
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.2
+  tmp = np.sum(w*x) + b
+  if tmp <= 0:
+   return 0
+  else:
+   return 1
+ def main():
+  print(NAND(0, 0))
+  print(NAND(0, 1))
+  print(NAND(1, 0))
+  print(NAND(1, 1))
+ main()  
