b889a71a41a9c3c3bcee1ad39bfaa839a21e0d2b
KSY_AI <suyun101014@gmail.com>
2025-09-20 21:36:02 +0900

    Update String.py

--- Baekjoon/step/String.py
+++ Baekjoon/step/String.py
@@ -28,3 +28,15 @@ import sys
 
 ch = sys.stdin.readline().strip()
 print(ord(ch))
+
+#11720
+import sys
+
+n = int(sys.stdin.readline().strip())
+numbers = sys.stdin.readline().strip()
+
+total = 0
+for digit in numbers:
+    total += int(digit)
+
+print(total)
